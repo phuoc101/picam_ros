d28d4a04091a9fe979fd03f29831d1ee2a33638f
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2022-08-29 15:22:21 +0300

    fixed export script

--- scripts/crop.py
+++ scripts/crop.py
@@ -1,27 +1,34 @@
-# import glob
+import glob
 import os
 import cv2
 import numpy as np
 import rospkg
 import rospy
+from rosbag.bag import Bag
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+
 
 class ImageCropper(object):
     def __init__(self):
         self.has_radius_and_center = False
-        self.lower_thresh = rospy.get_param("lower_thresh", default=150)
-        self.higher_thresh = rospy.get_param("higher_thresh", default=255)
-        self.image_topic = rospy.get_param(
-            "image_topic", default="/picam360/image_raw")
-        self.crop_topic = rospy.get_param(
-            "crop_topic", default="/picam360/crop")
+        self.lower_thresh = rospy.get_param("~lower_thresh", default=150)
+        self.higher_thresh = rospy.get_param("~higher_thresh", default=255)
+        self.image_topic = rospy.get_param("~image_topic")
         rospack = rospkg.RosPack()
         self.output_path = os.path.join(
             rospack.get_path("picam_ros"),
-            rospy.get_param("output_path", default="input_raw_picam360"))
-        self.image_subscriber = rospy.Subscriber(
-            self.image_topic, Image, self.image_cb)
+            "data",
+            rospy.get_param("~output_path", default="input_raw_picam360"))
+        print(rospy.get_param("~bagfile"))
+        self.bagfile = os.path.join(
+            rospack.get_path("picam_ros"),
+            "bags",
+            rospy.get_param("~bagfile"))
+        self.bag = Bag(self.bagfile, 'r')
+        self.img_count = self.bag.get_message_count("/picam360/image_raw")
+        os.makedirs(self.output_path, exist_ok=True)
+
         self.radius, self.center = None, None
         self.img_n = 0
         self.bridge = CvBridge()
@@ -53,17 +60,30 @@ class ImageCropper(object):
         filename = f"frame_{self.img_n:05d}.png"
         filepath = os.path.join(self.output_path, filename)
         cv2.imwrite(filepath, img_cropped)
+        rospy.loginfo(f"Saved {filepath}")
         self.img_n += 1
-        rospy.loginfo(f"Saved image {filepath}")
+    
+    def get_cropped_images(self):
+        for topic, msg, t in self.bag.read_messages(self.image_topic):
+            img_cv = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+            self.crop(img_cv)
+        self.bag.close()
 
-    def image_cb(self, msg):
-        img_cv = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-        self.crop(img_cv)
+    def get_video(self):
+        video_name = rospy.get_param("video_name", default="output.mp4")
+        video_dir = os.path.join(self.output_path, "video")
+        os.makedirs(video_dir, exist_ok=True)
+        video_path = os.path.join(video_dir, video_name)
+        os.system(
+            f"ffmpeg -r 30 -i {self.output_path}/frame_%05d.png -y {video_path}")
+        # rospy.loginfo(f"Saved video {video_path}")
+
 
 def main():
     rospy.init_node("image_cropper", anonymous=True, log_level=rospy.INFO)
     image_cropper = ImageCropper()
-    rospy.spin()
+    image_cropper.get_cropped_images()
+    image_cropper.get_video()
 
 
 if __name__ == "__main__":
